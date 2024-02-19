b072030830d5992b922cf3833a7b801c1a96980e
mmohammadzadeh <something@gmail.com>
2024-02-19 17:14:56 +0330

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,9 @@
-import { Button } from "@mui/material";
-import StyledButton from "./components/button/StyledButton";
-import Routes from "./router/Router";
+import React from 'react'
 
-import styled from "@emotion/styled";
-
-const StyledButtonn = styled.button`
-  background-color:teal;
-  color: #fff;
-  width: 250px;
-  height: 40px;
-  border-radius: 5px;
-  outline: none;
-  border: none;
-  font-size: 1.2rem;
-
-`;
 const App = () => {
   return (
-    <>
-      {/* <StyledButton
-        backgroundColor={"green"}
-        title={"click me"}
-        onClick={console.log("Clicked")}
-      />
-      <StyledButton
-        backgroundColor={"yellow"}
-        title={"Submit"}
-        onClick={handleSubmit}
-      />
-      <Routes /> */}
+    <div>App</div>
+  )
+}
 
-      <StyledButtonn>Click</StyledButtonn>
-    </>
-  );
-};
-export default App;
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+
+const StyledButtonn = styled.button`
+  background-color: teal;
+  color: #fff;
+  width: 250px;
+  height: 40px;
+  border-radius: 5px;
+  outline: none;
+  border: none;
+  font-size: 1.2rem;
+`;
+
+export default StyledButtonn;
