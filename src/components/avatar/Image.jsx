d97ccd8d0671813cbcda69f865246824f40d4bc7
mmohammadzadeh <something@gmail.com>
2024-03-03 12:51:45 +0330

    create Typography component and image component

---
+++ src/components/avatar/Image.jsx
@@ -0,0 +1,22 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const Image = ({src}) => {
+  return (
+    <Box
+    component="img"
+    sx={{
+      height: 75,
+      width: 75,
+      maxHeight: { xs: 75, md: 167 },
+      maxWidth: { xs: 75, md: 250 },
+      borderRadius: "50%",
+      boxShadow: "1px 2px 3px rgba(19, 19, 19, 0.5)",
+    }}
+    alt="user avatar"
+    src={src}
+  />
+  )
+}
+
+export default Image
--- src/components/myNotes/Note.jsx
+++ src/components/myNotes/Note.jsx
@@ -1,85 +1,88 @@
-import { Grid, Typography } from '@mui/material'
-import React from 'react'
+import { Grid } from "@mui/material";
+import React from "react";
+import { StyledText } from "../text/Text.styles";
 
 const Note = () => {
   return (
     <Grid item xs={12} container>
-    <Grid item xs={12} sx={{marginBottom:1}}>
-      <Typography style={{ color: "#fff" }}>02/10/2023</Typography>
+      <Grid item xs={12} sx={{ marginBottom: 1 }}>
+        <StyledText>02/10/2023</StyledText>
+      </Grid>
+      <Grid
+        item
+        container
+        xs={12}
+        sx={{
+          backgroundColor: "#1c2d3a",
+          color: "#fff",
+          borderRadius: 2,
+          padding: 2,
+          wordBreak: "break-all",
+        }}
+      >
+        <Grid item component={"div"} xs={12}>
+          <StyledText variant="textTitle">Note:</StyledText>
+        </Grid>
+        <Grid>
+          <StyledText component={"p"}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+            libero. Natus ipsam perferendis at architecto, culpa quia aliquid
+            enim aut qui tempore, odit possimus laudantium fugiat iste a tenetur
+            veritatis! Eaque accusamus doloremque quos. Quia excepturi, et
+            similique est libero modi facere magnam eius nostrum dolorum, unde
+            qui velit ullam quas corrupti, quisquam non distinctio. Nulla quasi
+            incidunt fuga, culpa est ad, voluptas omnis natus tenetur odio
+            nostrum impedit sunt numquam? Et culpa impedit animi ullam corporis
+            excepturi, dicta totam at ea voluptate voluptates dolorum eaque
+            placeat corrupti numquam tempora, nobis harum, atque enim debitis
+            repellat? Iste deleniti beatae mollitia voluptates fugiat, labore
+            eius vero suscipit odit! Aperiam quia facere, dolore ipsa eaque eos
+            architecto quam impedit quos tenetur laborum et error nobis
+            reprehenderit illum praesentium nisi veniam, natus modi quas commodi
+            suscipit? Natus cum consequatur ullam nostrum veritatis numquam non
+            quas voluptatibus cumque impedit provident possimus incidunt ut
+            saepe voluptatum quo alias recusandae quis eveniet, molestiae
+            assumenda iure quidem. Architecto sapiente numquam in, sit illum
+            neque molestias odit voluptatum qui exercitationem eveniet, vero
+            cupiditate autem deserunt reiciendis culpa dignissimos perferendis.
+            Provident, itaque alias autem ipsam voluptatem obcaecati numquam
+            dolor esse quaerat corporis assumenda minus illo aliquam distinctio
+            deleniti quam!
+          </StyledText>
+        </Grid>
+        <Grid item component={"div"} xs={12} sx={{ marginBlock: 1 }}>
+          <StyledText variant="textTitle">Medicine:</StyledText>
+        </Grid>
+        <Grid item component={"div"} xs={12}>
+          <StyledText component={"p"}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+            libero. Natus ipsam perferendis at architecto, culpa quia aliquid
+            enim aut qui tempore, odit possimus laudantium fugiat iste a tenetur
+            veritatis! Eaque accusamus doloremque quos. Quia excepturi, et
+            similique est libero modi facere magnam eius nostrum dolorum, unde
+            qui velit ullam quas corrupti, quisquam non distinctio. Nulla quasi
+            incidunt fuga, culpa est ad, voluptas omnis natus tenetur odio
+            nostrum impedit sunt numquam? Et culpa impedit animi ullam corporis
+            excepturi, dicta totam at ea voluptate voluptates dolorum eaque
+            placeat corrupti numquam tempora, nobis harum, atque enim debitis
+            repellat? Iste deleniti.
+          </StyledText>
+        </Grid>
+        <Grid item component={"div"} xs={12} sx={{ marginBlock: 1 }}>
+          <StyledText variant="textTitle">Homework:</StyledText>
+        </Grid>
+        <Grid item component={"div"} xs={12}>
+          <StyledText component={"p"}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
+            libero. Natus ipsam perferendis at architecto, culpa quia aliquid
+            enim aut qui tempore, odit possimus laudantium fugiat iste a tenetur
+            veritatis! Eaque accusamus doloremque quos. Quia excepturi, et
+            similique est libero modi facere magnam eius nostrum.
+          </StyledText>
+        </Grid>
+      </Grid>
     </Grid>
-    <Grid
-      item
-      container
-      xs={12}
-      sx={{ backgroundColor: "#1c2d3a", color: "#fff", borderRadius: 2,padding:2,wordBreak:'break-all' }}
-    >
-      <Grid item component={'div'} xs={12}>
-        <Typography variant="h6" component={'h2'} style={{color:"#f5b869"}}>Note:</Typography>
-      </Grid>
-      <Grid>
-        <Typography component={'p'} sx={{fontSize:{xs:'0.8rem', md:'1rem'}}}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
-          libero. Natus ipsam perferendis at architecto, culpa quia aliquid
-          enim aut qui tempore, odit possimus laudantium fugiat iste a
-          tenetur veritatis! Eaque accusamus doloremque quos. Quia
-          excepturi, et similique est libero modi facere magnam eius nostrum
-          dolorum, unde qui velit ullam quas corrupti, quisquam non
-          distinctio. Nulla quasi incidunt fuga, culpa est ad, voluptas
-          omnis natus tenetur odio nostrum impedit sunt numquam? Et culpa
-          impedit animi ullam corporis excepturi, dicta totam at ea
-          voluptate voluptates dolorum eaque placeat corrupti numquam
-          tempora, nobis harum, atque enim debitis repellat? Iste deleniti
-          beatae mollitia voluptates fugiat, labore eius vero suscipit odit!
-          Aperiam quia facere, dolore ipsa eaque eos architecto quam impedit
-          quos tenetur laborum et error nobis reprehenderit illum
-          praesentium nisi veniam, natus modi quas commodi suscipit? Natus
-          cum consequatur ullam nostrum veritatis numquam non quas
-          voluptatibus cumque impedit provident possimus incidunt ut saepe
-          voluptatum quo alias recusandae quis eveniet, molestiae assumenda
-          iure quidem. Architecto sapiente numquam in, sit illum neque
-          molestias odit voluptatum qui exercitationem eveniet, vero
-          cupiditate autem deserunt reiciendis culpa dignissimos
-          perferendis. Provident, itaque alias autem ipsam voluptatem
-          obcaecati numquam dolor esse quaerat corporis assumenda minus illo
-          aliquam distinctio deleniti quam!
-        </Typography>
-      </Grid>
-      <Grid item component={'div'} xs={12} sx={{marginBlock:1}}>
-        <Typography variant="h6" component={'h2'} style={{color:"#f5b869"}}>Medicine:</Typography>
-      </Grid>
-      <Grid item component={'div'} xs={12}>
-        <Typography component={'p'} sx={{fontSize:{xs:'0.8rem', md:'1rem'}}}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
-          libero. Natus ipsam perferendis at architecto, culpa quia aliquid
-          enim aut qui tempore, odit possimus laudantium fugiat iste a
-          tenetur veritatis! Eaque accusamus doloremque quos. Quia
-          excepturi, et similique est libero modi facere magnam eius nostrum
-          dolorum, unde qui velit ullam quas corrupti, quisquam non
-          distinctio. 
-        </Typography>
-      </Grid>
-      <Grid item component={'div'} xs={12} sx={{marginBlock:1}}>
-        <Typography variant="h6" component={'h2'} style={{color:"#f5b869"}}>Homework:</Typography>
-      </Grid>
-      <Grid item component={'div'} xs={12}>
-        <Typography component={'p'} sx={{fontSize:{xs:'0.8rem', md:'1rem'}}}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde,
-          libero. Natus ipsam perferendis at architecto, culpa quia aliquid
-          enim aut qui tempore, odit possimus laudantium fugiat iste a
-          tenetur veritatis! Eaque accusamus doloremque quos. Quia
-          excepturi, et similique est libero modi facere magnam eius nostrum
-          dolorum, unde qui velit ullam quas corrupti, quisquam non
-          distinctio.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, sequi.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, sequi.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, sequi.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, sequi.
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, sequi.
-        </Typography>
-      </Grid>
-    </Grid>
-  </Grid>
-  )
-}
+  );
+};
 
-export default Note
+export default Note;
